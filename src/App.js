14ae4291e9b7c9b103ea2cf9e2cd701d4eb5061e
Priyaraj7 <suppichala@gmail.com>
2021-09-07 12:27:24 -0700

    add user component

--- src/App.js
+++ src/App.js
@@ -1,49 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Footer from "./components/footer";
+import Header from "./components/Header";
+import Users from "./components/Users";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <img src="./calendar.png" alt="Calendar Star Logo" />
-        <h1>Eventonica</h1>
-      </header>
-
+      <Header />
       <main>
         <div className="user-and-events">
-          <section className="user-management">
-            <h2>User Management</h2>
-
-            <ul id="users-list">
-              {/* display all existing Users here */}
-              <li>...</li>
-            </ul>
-
-            <div>
-              <h3>Add User</h3>
-              <form id="add-user" action="#">
-                <fieldset>
-                  <label>Name</label>
-                  <input type="text" id="add-user-name" />
-                </fieldset>
-                {/* Add more form fields here */}
-                <input type="submit" value="Add" />
-              </form>
-            </div>
-
-            <div>
-              <h3>Delete User</h3>
-              <form id="delete-user" action="#">
-                <fieldset>
-                  <label>User ID</label>
-                  <input type="text" id="delete-user-id" />
-                </fieldset>
-                <input type="submit" />
-              </form>
-            </div>
-          </section>
-
+          <Users />
           <section className="event-management">
             <h2>Event Management</h2>
             <div>

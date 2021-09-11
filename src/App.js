cc8c241b852aec6d9d7d50f139a8b10006d15869
Priyaraj7 <suppichala@gmail.com>
2021-09-11 15:41:07 -0700

    add FindEventForm

--- src/App.js
+++ src/App.js
@@ -16,34 +16,8 @@ function App() {
         </div>
 
         {/* <DeleteUser /> */}
-        <div>
-          <h3>Delete Event</h3>
-          <form id="delete-event" action="#">
-            <fieldset>
-              <label>Event ID</label>
-              <input type="number" min="1" id="delete-event-id" />
-            </fieldset>
-            <input type="submit" />
-          </form>
-        </div>
-
-        <aside className="search-toolbar">
-          <div>
-            <h3>Find Events</h3>
-            <form id="search" action="#">
-              <fieldset>
-                <label htmlFor="date-search">Date</label>
-                <input type="text" id="date-search" placeholder="YYYY-MM-DD" />
-              </fieldset>
-              <fieldset>
-                <label htmlFor="category-search">Category</label>
-                <input type="text" id="category-search" />
-              </fieldset>
-
-              <input type="submit" value="Search" />
-            </form>
-          </div>
-        </aside>
+        {/* deleteEvent */}
+        {/* findEvent */}
       </main>
       <Footer />
     </div>
--- src/components/Event.js
+++ src/components/Event.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import EventForm from "./EventForm";
+import DeleteEvent from "./DeleteEvent";
+import FindEvent from "./FindEvent";
 
 const Events = () => {
   // mock State
   const event1 = {
-    id: 1,
+    id: "1",
     name: "Birthday",
     date: "2021-09-01",
     description: "A birthday party for my best friend",
@@ -12,7 +14,7 @@ const Events = () => {
   };
 
   const event2 = {
-    id: 2,
+    id: "2",
     name: "Graduation",
     date: "2021-08-01",
     description: "The class of 2021 graduates from East High",
@@ -20,7 +22,7 @@ const Events = () => {
   };
 
   const event3 = {
-    id: 3,
+    id: "3",
     name: "JS Study Session",
     date: "2021-10-01",
     description: "A chance to practice Javascript interview questions",
@@ -33,27 +35,47 @@ const Events = () => {
     setEvents([...events, newEvent]);
   };
 
+  // delete event
+  const handleDeleteEvent = (deleteId) => {
+    //debugger;
+    const deleteEvent = events.filter((event) => event.id !== deleteId);
+    setEvents(deleteEvent);
+  };
+
+  //Search Event
+  const handleSearchEvent = (category, date) => {
+    //debugger;
+    const findEvent = events.filter(
+      (event) => event.category === category || event.date === date
+    );
+    setEvents(findEvent);
+  };
+
   return (
-    <section className="event-management">
-      <h2>Event Management</h2>
-      <div>
-        <h3>All Events</h3>
-        <ul id="events-list">
-          {/* Display all Events here */}
-          {events.map((eve, i) => (
-            <li key={i}>
-              <strong>Name:</strong> {eve.name}
-              <strong>Date:</strong> {eve.date}
-              <strong>Description:</strong> {eve.description}
-              <strong>Category:</strong> {eve.category}
-              <strong>Id:</strong> {eve.id}
-            </li>
-          ))}
-        </ul>
-        <EventForm handleSubmitAddEvent={handleSubmitAddEvent} />
-        {/* */}
-      </div>
-    </section>
+    <>
+      <section className="event-management">
+        <h2>Event Management</h2>
+        <div>
+          <h3>All Events</h3>
+          <ul id="events-list">
+            {/* Display all Events here */}
+            {events.map((eve, i) => (
+              <li key={i}>
+                <strong>Name:</strong> {eve.name}
+                <strong>Date:</strong> {eve.date}
+                <strong>Description:</strong> {eve.description}
+                <strong>Category:</strong> {eve.category}
+                <strong>Id:</strong> {eve.id}
+              </li>
+            ))}
+          </ul>
+          <EventForm handleSubmitAddEvent={handleSubmitAddEvent} />
+          <DeleteEvent handleDeleteEvent={handleDeleteEvent} />
+          <FindEvent handleSearchEvent={handleSearchEvent} />
+          {/* */}
+        </div>
+      </section>
+    </>
   );
 };
 
